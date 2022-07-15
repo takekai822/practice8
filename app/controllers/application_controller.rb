c215f89094d4396f8f205bf990b6c63ca2f017da
takekai822 <kaikisoccer78@gmail.com>
2022-07-15 08:35:38 +0000

    deviseインストール

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameter, if: :devise_controller?
+  
+  protected
+  def configure_permitted_parameter
+    devise_parameter_sanitizer.permit(:sign_up, keys:[:email])
+  end
 end
