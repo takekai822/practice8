e5e02927b7958dd53ba6935915f1c080d52aac25
takekai822 <kaikisoccer78@gmail.com>
2022-07-15 08:41:24 +0000

    deviseのviewページの変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,14 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameter, if: :devise_controller?
   
+  def after_user_sign_in_path_for(resource)
+    root_path
+  end
+  
+  def after_user_sign_out_path_for(resource)
+    root_path
+  end
+  
   protected
   def configure_permitted_parameter
     devise_parameter_sanitizer.permit(:sign_up, keys:[:email])
