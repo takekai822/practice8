2a8b5cc034eae79752e5daef341126b7f1c97114
takekai822 <kaikisoccer78@gmail.com>
2022-07-30 00:47:56 +0000

    いいね機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   get 'home/about' => "homes#about", as: "about"
   devise_for :users
-  resources :books, except: [:new]
+  resources :books, except: [:new] do
+    resource :favorites, only: [:create, :destroy]
+  end
   resources :users, except: [:new, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
